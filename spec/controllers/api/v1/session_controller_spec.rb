237b79d483ffc0bb80b3cc6d8e5d2c98e67e8367
devror007 <devror007@gmail.com>
2017-10-06 19:15:29 +0530

    added rpsec for session

---
+++ spec/controllers/api/v1/session_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe Api::V1::SessionsController, type: :controller do
+  describe "Post 'create'" do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'user not found' do
+      post :create
+      expect(json).to eq({"message"=>"User not found"})
+    end
+
+    it 'not valid password' do
+      post :create, {email: @user.email, password: 'A12345678' }
+      expect(json).to eq({"message"=>"User credentails are not valid"})
+    end
+
+    it 'should not set auth token' do
+      user = FactoryGirl.create(:user, auth_token: nil)
+      allow_any_instance_of(User).to receive(:generate_authentication_token!).and_return(nil)
+      post :create, {email: user.email, password: user.password }
+      expect(json).to eq({"message"=>"Could not set auth toekn"})
+    end
+
+    it 'success' do
+      user = FactoryGirl.create(:user, auth_token: nil)
+      post :create, {email: user.email, password: user.password }
+      expect(json).to eq({"email"=>user.email, "auth_token"=>user.reload.auth_token})
+    end
+  end
+end
